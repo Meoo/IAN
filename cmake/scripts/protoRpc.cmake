110f0b9bcc5ef4f3d14262ea4c20cce9be44727e
Bastien Brunnenstein <bastien@brunnenstein.fr>
2018-02-25 23:46:21 +0100

    Args in RPC

--- cmake/scripts/protoRpc.cmake
+++ cmake/scripts/protoRpc.cmake
@@ -25,7 +25,53 @@ macro(rpc_function)
   set(multiArgs IN OUT)
   cmake_parse_arguments(ARG "${opts}" "${oneArgs}" "${multiArgs}" ${ARGN})
 
+  if(NOT RPC_CURRENT_IF)
+    message(FATAL_ERROR "rpc_function called with no active interface")
+  endif()
+
+  if(NOT ARG_NAME)
+    message(FATAL_ERROR "rpc_function called with no name in ${RPC_CURRENT_IF}")
+  endif()
+
   list(APPEND RPC_D_${RPC_CURRENT_IF}_FUNCTIONS ${ARG_NAME})
+
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_TYPES)
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_VARS)
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_PAIRS)
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_TYPES)
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_VARS)
+  set(RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_PAIRS)
+
+  set(NEXT_VAR OFF)
+  foreach(IN_V ${ARG_IN})
+    if(NOT NEXT_VAR)
+      set(IN_T ${IN_V})
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_TYPES ${IN_V})
+      set(NEXT_VAR ON)
+    else()
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_VARS ${IN_V})
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_IN_PAIRS "${IN_T} ${IN_V}")
+      set(NEXT_VAR OFF)
+    endif()
+  endforeach()
+  if(NEXT_VAR)
+    message(FATAL_ERROR "Malformed IN for ${RPC_CURRENT_IF}.${ARG_NAME}")
+  endif()
+
+  foreach(OUT_V ${ARG_OUT})
+    if(NOT NEXT_VAR)
+      set(OUT_T ${OUT_V})
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_TYPES ${OUT_V})
+      set(NEXT_VAR ON)
+    else()
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_VARS ${OUT_V})
+      list(APPEND RPC_D_${RPC_CURRENT_IF}_${ARG_NAME}_OUT_PAIRS "${OUT_T} ${OUT_V}")
+      set(NEXT_VAR OFF)
+    endif()
+  endforeach()
+  if(NEXT_VAR)
+    message(FATAL_ERROR "Malformed OUT for ${RPC_CURRENT_IF}.${ARG_NAME}")
+  endif()
 endmacro()
 
 
@@ -34,6 +80,11 @@ endmacro()
 include(${RPC_DESCRIPTOR})
 
 #
+
+
+if(NOT RPC_NAMESPACE)
+  message(FATAL_ERROR "A namespace must be defined")
+endif()
 
 
 # Generate .fbs
@@ -57,7 +108,15 @@ endforeach()
 
 # Generate .hpp
 macro(write_fun_receiver FILE IF FUN)
-  file(APPEND "${FILE}" "  virtual void ${FUN}() = 0;\n")
+  set(FUN_ARGS ${RPC_D_${IF}_${FUN}_IN_PAIRS})
+  set(REP_TYPES ${RPC_D_${IF}_${FUN}_OUT_TYPES})
+  if(NOT REP_TYPES)
+    set(REP_TYPES "void")
+  endif()
+  string(REPLACE ";" ", " REP_TYPES_LIST "${REP_TYPES}")
+  list(INSERT FUN_ARGS 0 "ReturnCallback<${REP_TYPES_LIST}> & callback")
+  string(REPLACE ";" ", " ARG_LIST "${FUN_ARGS}")
+  file(APPEND "${FILE}" "  virtual void ${FUN}(${ARG_LIST}) = 0;\n")
 endmacro()
 
 macro(write_fun_sender FILE IF FUN)
@@ -71,6 +130,7 @@ file(APPEND "${RPC_HEADER_OUT}" "namespace ${RPC_NAMESPACE}\n{\n\n")
 foreach(IF ${RPC_IF_LIST})
   file(APPEND "${RPC_HEADER_OUT}" "class ${IF}Receiver\n{\n")
   file(APPEND "${RPC_HEADER_OUT}" "public:\n")
+  file(APPEND "${RPC_HEADER_OUT}" "  using Request = ${IF}Request;\n")
 
   # Loop on functions
   foreach(FUN ${RPC_D_${IF}_FUNCTIONS})
@@ -79,7 +139,7 @@ foreach(IF ${RPC_IF_LIST})
 
   file(APPEND "${RPC_HEADER_OUT}" "protected:\n")
   file(APPEND "${RPC_HEADER_OUT}" "  template<typename F>\n")
-  file(APPEND "${RPC_HEADER_OUT}" "  void dispatch_rpc(const ${IF}Request & request, F & callback) {}\n")
+  file(APPEND "${RPC_HEADER_OUT}" "  void dispatch_rpc(const Request & request, F & callback) {}\n")
   file(APPEND "${RPC_HEADER_OUT}" "};\n\n")
   file(APPEND "${RPC_HEADER_OUT}" "class ${IF}Sender\n{\npublic:\n")
 
