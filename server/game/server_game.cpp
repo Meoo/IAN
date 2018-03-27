1f18c8bf6078011d8dad1955b6fdd5c1452abd07
Bastien Brunnenstein <bastien@brunnenstein.fr>
2018-03-27 19:43:50 +0200

    Remove all server module related code & files

--- server/game/server_game.cpp
+++ server/game/server_game.cpp
@@ -1,10 +1,2 @@
 
-#include <module-bundle/ModuleBundle.hpp>
-
-
-int main(int argc, char ** argv)
-{
-  modbundle::load_modules(nullptr);
-
-  return 0;
-}
+int main(int argc, char ** argv) { return 0; }
